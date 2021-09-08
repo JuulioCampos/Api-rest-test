6a7a8542c1d71f1020896a61739613ccd748a30d
Julio Campos <julio.campos2@fatec.sp.gov.br>
2021-09-08 20:30:11 -0300

    Ajustando rotas e api

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Student;
 use Illuminate\Http\Request;
 
@@ -13,18 +14,7 @@ class StudentController extends Controller
      */
     public function index()
     {
-       return Student::get();
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+       return response()->json(Student::get(), 200);
     }
 
     /**
@@ -35,7 +25,29 @@ class StudentController extends Controller
      */
     public function show($id)
     {
-        //
+        $result = Student::find($id);
+
+        if(!$result) return response()->json(['message'=>'not found'], 404);
+
+        return response()->json($result, 302) ;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            return Student::create($request->all());
+        } catch (\Throwable $e) {
+            return response()->json([
+                'message'=> $e->getMessage()
+            ],500);
+        }
+        // dd($request->all());
     }
 
     /**

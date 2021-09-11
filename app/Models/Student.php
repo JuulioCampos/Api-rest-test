b96643dbee01f3c251c68ace54bb5eb148e920cc
Julio Campos <julio.campos2@fatec.sp.gov.br>
2021-09-11 10:19:01 -0300

    Criando assessores

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,17 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use League\CommonMark\Extension\Attributes\Node\Attributes;
 
 class Student extends Model
 {
     use HasFactory;
-    protected $fillable=['name', 'birth','gender','classroom_id'];
+    protected $fillable=['name', 'birth','gender','classroom_id', 'sex'];
 
+    protected $casts = [
+        'birth' => 'datetime:d/m/Y',
+    ];
+    public $appends = ['define_gender'];
+    protected $visible = [
+        'name',
+        'birth',
+        'define_gender',
+        'classroom_id'
+    ];
     /**
-     * map. com salas de aulas 
+     * map. com salas de aulas
      */
     public function classroom()
     {
         return $this->belongsTo('App\Models\Classroom');
+    }
+
+    public function getDefineGenderAttribute()
+    {
+        return $this->attributes['gender'] === 'F' ? 'Feminino' : 'Masculino';
     }
 }
